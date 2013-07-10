c15f6b807016f3ec06472c9162b63e6b69a22a16
Jacob Danner <jacob.danner@gmail.com>
2013-07-09 20:49:10 -0700

    adding basic checks for grader.js

---
+++ grader.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+/*
+ * Automatically grade files for presence of specified HTML tags/attributes.
+ * Uses commander.js and cheerio. teaches command line application development 
+ * and basic DOM Parsing
+ *
+ * References:
+ *  Cheerio 
+ *  Commander.js
+ *  JSON
+ */
+
+var fs = require('fs');
+var program = require('commander');
+var cheerio = require('cheerio');
+var HTMLFILE_DEFAULT="index.html";
+var CHECKSFILE_DEFAULT="checks.json";
+
+var assertFileExists = function(infile) {
+  var instr = infile.toString();
+  if(!fs.existsSync(instr))
+  {
+    console.log("%s does not exist. Exiting.", instr);
+    process.exit(1);
+  }
+  return instr;
+};
+
+var cheerioHtmlFile = function(htmlFile){
+  return cheerio.load(fs.readFileSync(htmlFile));
+};
+
+var loadChecks = function(checksFile){
+  return JSON.parse(fs.readFileSync(checksFile));
+};
+
+var checkHtmlFile = function(htmlFile, checksFile) {
+  $ = cheerioHtmlFile(htmlFile);
+  var checks = loadChecks(checksFile);
+  var out = {};
+
+  for(var ii in checks)
+  {
+    var present = $(checks[ii]).length > 0;
+    out[check[ii]] = present;
+  }
+  return out;
+};
+
+var clone = function(fn) {
+  // Commander issue workaround -> SO 6772648
+  return fn.bind({});
+};
+
+
+if(require.main == module) {
+  program
+    .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+    .option('-f, --file <check_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .parse(process.argv);
+  var checkJSON = checkHtmlFile(program.file, program.checks);
+  var outJSON = JSON.stringify(checkJSON, null, 4);
+  console.log(outJSON);
+} else {
+  exports.checkHtmlFile = checkHtmlFile;
+}
+
+
+
+
+
