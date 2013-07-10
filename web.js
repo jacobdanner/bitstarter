0e5d72a3a6f8a4f9870df8b13dfffe22c854d4f0
Jacob Danner <jacob.danner@oracle.com>
2013-07-09 20:19:54 -0700

    added bootstrap, css, and modified express to use path

--- web.js
+++ web.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var path = require("path");
+var http = require("http");
+
+
 
 var app = express.createServer(express.logger());
+
+app.configure(function() {
+  app.use(express.static(path.join(__dirname,'public')));
+});
 
 app.get('/', function(request, response) {
     response.send(fs.readFileSync("index.html","utf-8"));
